ea5c278fed14d435cf63bf147b0f1df55a20c342
Rodrigo Nascimento <rodrigofn0@hotmail.com>
2019-07-22 12:58:55 -0300

    Add IO code

--- rodrigonascimento_201600155174_poximdb.java
+++ rodrigonascimento_201600155174_poximdb.java
@@ -1,6 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 public class rodrigonascimento_201600155174_poximdb {
+
+    /**
+     * Writes content to file.
+     * @param fileName  Name of the file (with extension) to be writen.
+     * @param content   Content to be writen on the file.
+     * @throws FileNotFoundException
+     */
+    private static void writeToFile(String fileName, String content) throws FileNotFoundException {
+
+        try(FileWriter fw = new FileWriter(fileName, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.print(content);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         
+        try (FileInputStream inputStream = new FileInputStream(args[0])) {
+            
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
     }
 }
