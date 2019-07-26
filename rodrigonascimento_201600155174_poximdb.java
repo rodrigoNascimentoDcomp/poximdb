8e566faca1a05b19e5553c0232f674fa7f8bb15b
Rodrigo Nascimento <rodrigofn0@hotmail.com>
2019-07-26 13:12:34 -0300

    Add insertion into filled up child

--- rodrigonascimento_201600155174_poximdb.java
+++ rodrigonascimento_201600155174_poximdb.java
@@ -44,13 +44,6 @@ public class rodrigonascimento_201600155174_poximdb {
     public static Tree tree;
     public static int order;
 
-    /*public static Node[] orderNodes(Node[] nodes) {
-        Node aux;
-        for (int i = 0; i < nodes.length; i++) {
-
-        }
-    }*/
-
     /**
      * Reads the line of the input with the file information
      * and returns a node containing it.
@@ -79,6 +72,27 @@ public class rodrigonascimento_201600155174_poximdb {
     }
 
     /**
+     * Inserts a new node into a Node array.
+     * 
+     * @param nodes     The array into which the new node shall be added.
+     * @param newNode   The new node that will be added to the array.
+     * @return          The array with the added node.
+     */
+    public static Node[] insertNode(Node[] nodes, Node newNode) {
+        Node aux;
+        for (int i = 0; i < nodes.length; i++) {
+            if (nodes[i] == null) {
+                nodes[i] = newNode;
+            } else if (newNode.key.compareTo(nodes[i].key) < 0) {
+                aux = nodes[i];
+                nodes[i] = newNode;
+                newNode = aux;
+            }
+        }
+        return nodes;
+    }
+
+    /**
      * Inserts a new node into the tree.
      * 
      * @param newNode Node to be inserted on the tree.
@@ -102,44 +116,82 @@ public class rodrigonascimento_201600155174_poximdb {
 
                     if (tree.keys[i] == null) {
                         tree.keys[i] = newNode;
-                    } else if (newNode.key.compareTo(tree.keys[i].key) < 0) {
+                        return tree;
+                    } else if (newNode.key.compareTo(tree.keys[i].key) < 0
+                                || newNode.key.compareTo(tree.keys[i].key) == 0) {
                         
                         Node auxNode = tree.keys[i];
                         tree.keys[i] = newNode;
                         return insertNode(tree, auxNode);
 
                     }
-        
                 }
 
                 return tree;
-            }
 
-            // If the keys are full, divide
-            if (tree.keys[tree.order - 2] != null) {
+            } else {
+                // Create a new Tree
+
+                Node[] nodeArray = new Node[tree.order];
+
+                // TODO: find a better way to copy the array
+                for (int i = 0; i < tree.order - 1; i++) {
+                    nodeArray[i] = tree.keys[i];
+                }
                 
-            }
+                nodeArray = insertNode(nodeArray, newNode);
+                int middleIndex = (int) nodeArray.length / 2;
 
-            return tree;
+                Tree newTree = new Tree(tree.order);
+                Tree leftChild = new Tree(tree.order);
+                Tree rightChild = new Tree(tree.order);
+
+                // Makes the middle node of the array
+                // the new parent node
+                newTree.keys[0] = nodeArray[middleIndex];
+
+                // Copies the first half of the node array
+                // into the left child
+                for (int i = 0; i < middleIndex; i++) {
+                    leftChild.keys[i] = nodeArray[i];
+                }
+
+                // Copies the second half of the array
+                // into the right child
+                int j = 0;
+                for (int i = middleIndex + 1; i < nodeArray.length; i++) {
+                    rightChild.keys[j] = nodeArray[i];
+                    j++;
+                }
+
+                // Adds the children to the tree
+                newTree.children[0] = leftChild;
+                newTree.children[1] = rightChild;
+
+                return newTree;
+            }
 
         } else {
+            // Go down
 
             for (int i = 0; i < tree.order - 1; i++) {
 
-                // Go to the left children if the current key is null or bigger than the new one
-                if (tree.keys[i] == null || newNode.key.compareTo(tree.keys[i].key) < 0) {
+                // Go to the left children if the current key
+                // is null or bigger or equal than the new one
+                if (tree.keys[i] == null
+                    || newNode.key.compareTo(tree.keys[i].key) < 0
+                    || newNode.key.compareTo(tree.keys[i].key) == 0) {
 
-                    return insertNode(tree.children[i], newNode);
+                    tree.children[i] = insertNode(tree.children[i], newNode);
 
                 } else if (i == tree.order - 2) {
                     // Go right if it's the last key and the new node is bigger
 
-                    return insertNode(tree.children[tree.order - 1], newNode);
+                    tree.children[tree.order - 1] = insertNode(tree.children[tree.order - 1], newNode);
                 }
             }
 
             return tree;
-
         }
     }
 
